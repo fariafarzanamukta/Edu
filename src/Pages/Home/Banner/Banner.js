cc4bae22829e576d89d9b1da55a971567a2d954f
NieamulKabir <kabir000079@gmail.com>
2022-10-11 18:46:33 +0600

    add banner

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -4,15 +4,11 @@ import reader from '../../../assets/reader.json'
 
 const Banner = () => {
     return (
-        <div className='px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-24'>
+        <div className='px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-2'>
         <div className='flex flex-col items-center justify-between lg:flex-row'>
           <div className='mb-10 lg:max-w-lg  lg:pr-5 lg:mb-0'>
-            <div className='max-w-xl mb-6 lg:mt-8'>
-              <div>
-                <p className='inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-yellow-400'>
-                  On Sale!
-                </p>
-              </div>
+            <div className='max-w-xl mb-6 lg:mt-2'>
+              {/* <h1>That Quiz is a website featuring multiple-choice, matching, and short-answer tests on a variety of core subjects.</h1> */}
               <h2 className='max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none'>
                 A reader lives a <br className='hidden md:block' /> thousand lives{' '}
                 <span className='inline-block text-blue-400'>before he dies</span>
